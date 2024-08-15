f50bc8d9d5465dc4fbbff07f2fc54106ce4396d4
Manpreet Kaur <kaur0959@algonquinlive.com>
2024-08-15 09:15:04 -0400

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import ContactDetailView from '../views/ContactDetailView.vue';
+
 import ContactFormView from '../views/ContactFormView.vue';
 
 const routes = [
